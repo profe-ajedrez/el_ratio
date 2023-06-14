6530e34b98d366aaaf94b858634ac2f440899fae
Andrés Reyes a.k.a. Jacobopus <chess.coach.ar@gmail.com>
2023-06-14 00:27:39 -0400

    tidy up

---
+++ examples/chiexample/chi.go
@@ -0,0 +1,62 @@
+package chiexample
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/profe-ajedrez/el_ratio/"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+var l *rater.LeakybuckerLimiter
+
+func main() {
+
+	l = rater.NewLeakyBucketLimiter(1, 5*time.Second)
+
+	r := chi.NewRouter()
+	r.Use(Limiter)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	http.ListenAndServe(":3333", r)
+}
+
+var channel chan func() = make(chan func(), 1)
+
+func QueueLimiter(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+
+		channel <- func() {
+			for len(channel) > 0 && len(channel) == cap(channel) {
+				fmt.Print(".")
+				time.Sleep(16 * time.Millisecond)
+			}
+
+			ctx := r.Context()
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func Limiter(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+
+		now := l.Wait()
+
+		fmt.Println(now)
+		ctx := r.Context()
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+	return http.HandlerFunc(fn)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module el_ratio
+
+go 1.20
+
+require (
+	code.cloudfoundry.org/clock v1.1.0
+	github.com/go-chi/chi/v5 v5.0.8
+
+)
